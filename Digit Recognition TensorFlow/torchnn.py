61d2fc1f2ed3c78742344229c783fe81658b51b7
henrym105 <henrym105@gmail.com>
2023-12-02 23:41:12 -0500

    added command line image selection capability

--- Digit Recognition TensorFlow/torchnn.py
+++ Digit Recognition TensorFlow/torchnn.py
@@ -5,11 +5,12 @@ from torch.optim import Adam
 # to load datsets
 from torch.utils.data import DataLoader
 from torchvision import datasets
-from torchvision.transforms import ToTensor
+from torchvision.transforms import ToTensor, Compose
 
 # Used to load model after initial training
 import torch
 from PIL import Image
+import os, sys
 
 # import dataset
 train = datasets.MNIST(root = "data", download=True, train=True, transform=ToTensor())
@@ -41,32 +42,52 @@ clf = ImageClassifier().to('cpu')
 opt = Adam(clf.parameters(), lr=1e-3)
 loss_fn = nn.CrossEntropyLoss()
 
+def train_nn():
+    # local cpu training ~ 15 minutes
+    print("\n\nBeginning the training process...")
+    for epoch in range(10):
+        for batch in dataset:
+            X,y = batch
+            X,y = X.to('cpu'), y.to('cpu')
+            # make prediction
+            yhat = clf(X)
+            loss = loss_fn(yhat, y)
+
+            # Apply backpro
+            opt.zero_grad() # zero out gradient
+            loss.backward() # calculate gradients
+            opt.step()
+
+        print(f"Epoch: {epoch} loss is {loss.item()}")
+
+    with open('model_state.pt', 'wb') as f:
+        torch.save(clf.state_dict(), f)
+
 # training function
 if __name__ == "__main__":
-    with open('model_state.pt', 'rb') as f:
-        clf.load_state_dict(load(f))
+    # train the model if the state doesn't exist:
+    if not(os.path.exists('model_state.pt')):
+        train_nn()
 
-    img = Image.open('img_9.jpg')
+    # load the stored pytorch nn
+    with open('model_state.pt', 'rb') as f:
+        clf.load_state_dict(torch.load(f))
+
+    # Load the RGB image
+    if len(sys.argv) > 1:
+        img = Image.open(f"{sys.argv[1]}.jpg")
+    else:
+        # default to image 1 if no arg is given
+        img = Image.open('img_1.jpg')
+
+    # Convert the image to grayscale
+    img = img.convert(mode='L')
+    # resize the image to the appropriate size (based on the MNIST training set)
+    img = img.resize((28, 28))
+
+    # img = Image.open('img_9.jpg')
+    # transform = Compose([ToTensor(),])
+
     img_tensor = ToTensor()(img).unsqueeze(0).to('cpu')
 
     print(torch.argmax(clf(img_tensor)))
-
-    # # local cpu training ~ 15 minutes
-    # print("\n\nBeginning the training process...")
-    # for epoch in range(10):
-    #     for batch in dataset:
-    #         X,y = batch
-    #         X,y = X.to('cpu'), y.to('cpu')
-    #         # make prediction
-    #         yhat = clf(X)
-    #         loss = loss_fn(yhat, y)
-
-    #         # Apply backpro
-    #         opt.zero_grad() # zero out gradient
-    #         loss.backward() # calculate gradients
-    #         opt.step()
-
-    #     print(f"Epoch: {epoch} loss is {loss.item()}")
-
-    # with open('model_state.pt', 'wb') as f:
-    #     save(clf.state_dict(), f)
